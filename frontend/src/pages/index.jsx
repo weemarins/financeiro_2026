4d2efc907f1ae5db79237b394630f23c3bdc5334
Weberte Alcedino Marins de Oliveira <weberte.oliveira@detran.df.gov.br>
2026-03-24 12:25:21 -0300

    Alteração do index.jsx

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { MainLayout } from '../components/Layout.jsx';
+import { DashboardPage as Dashboard } from './DashboardPage.jsx';
+
+export { Dashboard as DashboardPage };
 
 export function TransactionsPage() {
   return (
